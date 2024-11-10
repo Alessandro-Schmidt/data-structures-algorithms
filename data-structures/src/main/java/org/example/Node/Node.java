3d43b810fae1387bafa10c8d1ae9504d49c57261
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2024-11-10 02:01:43 -0300

    Creation of Node class

---
+++ data-structures/src/main/java/org/example/Node/Node.java
@@ -0,0 +1,27 @@
+package org.example.Node;
+
+public class Node {
+    int data;
+    Node next;
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node(int data) {
+        this.data = data;
+        setNext(null);
+    }
+}
